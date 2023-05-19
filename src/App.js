00933b0fca013d3951ffd399f20e1b83f513d1ef
anyangjin <ayz0155@gmail.com>
2023-05-19 18:03:36 +0900

    Router 사용 page 이동 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import LoginPage from "./pages/LoginPage/LoginPage";
 import StartPage from "./pages/StartPage/StartPage";
+import AppRouter from "./pages/Router";
 
 function App() {
   return (
     <div>
-      <StartPage />
+      <AppRouter />
     </div>
   );
 }
