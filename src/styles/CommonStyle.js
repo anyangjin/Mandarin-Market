9ba5a227da72400362cbab4fc859ca1083feb56c
anyangjin <ayz0155@gmail.com>
2023-07-26 15:52:30 +0900

    Mandarine-market 시작

---
+++ src/styles/CommonStyle.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const styledWrapper = styled.div`
+  margin: 0 auto;
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
